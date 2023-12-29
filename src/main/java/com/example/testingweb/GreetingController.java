c8419a3db7fce6b8cff0a09f9dd63b705fbc7b33
josezumba <105942535+josezumba@users.noreply.github.com>
2023-12-28 19:44:02 -0500

    Update GreetingController.java

--- src/main/java/com/example/testingweb/GreetingController.java
+++ src/main/java/com/example/testingweb/GreetingController.java
@@ -3,6 +3,7 @@ package com.example.testingweb;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -17,6 +18,12 @@ public class GreetingController {
 	@RequestMapping("/greeting")
 	public @ResponseBody String greeting() {
 		return service.greet();
+	}
+
+	@RequestMapping("/greeting1")
+	public @ResponseBody String greeting1(@RequestParam(value = "name", defaultValue = "World") String name) {
+		String query = "SELECT * FROM users WHERE username = '" + name + "'";
+		return service.greet1(query);
 	}
 
 }
