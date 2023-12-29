228a244d1cf7bc581d7145b2d78c53dbbc327a68
josezumba <105942535+josezumba@users.noreply.github.com>
2023-12-28 19:42:45 -0500

    Update GreetingService.java

--- src/main/java/com/example/testingweb/GreetingService.java
+++ src/main/java/com/example/testingweb/GreetingService.java
@@ -7,5 +7,7 @@ public class GreetingService {
 	public String greet() {
 		return "Hola, soy José Rolando Zumba Gómez";
 	}
-
+public String greet1(String query) {
+		return "Iyección código" + query;
+	}
 }
